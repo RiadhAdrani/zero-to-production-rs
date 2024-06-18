4c7b3d2e44b2d4bde3d7b6b06fc2e98efc8d800e
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-06-18 14:26:33 +0100

    feat: add health_check and subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero_to_prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
